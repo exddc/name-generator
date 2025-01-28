c441ac8ec6457f710334bbcea40669ee532f97f2
Timo Weiss <timo.weiss@protonmail.ch>
2025-01-28 13:48:00 +0100

    url changes

--- backend/src/backend/utils.py
+++ backend/src/backend/utils.py
@@ -131,19 +131,19 @@ def check_services_connections(session) -> str:
     """
     services = []
     try:
-        resp = requests.get(DOMAIN_CHECKER_URL + "/health", timeout=5)
+        resp = requests.get(DOMAIN_CHECKER_URL + "health", timeout=5)
         resp.raise_for_status()
     except requests.exceptions.RequestException as e:
         print(msg := f"Error connecting to domain-checker: {e}")
-        print(f"Used URL: {DOMAIN_CHECKER_URL + '/health'}")
+        print(f"Used URL: {DOMAIN_CHECKER_URL + 'health'}")
         services.append(msg)
 
     try:
-        resp = requests.get(NAME_SUGGESTOR_URL + "/health", timeout=5)
+        resp = requests.get(NAME_SUGGESTOR_URL + "health", timeout=5)
         resp.raise_for_status()
     except requests.exceptions.RequestException as e:
         print(msg := f"Error connecting to name-suggestor: {e}")
-        print(f"Used URL: {NAME_SUGGESTOR_URL + '/health'}")
+        print(f"Used URL: {NAME_SUGGESTOR_URL + 'health'}")
         services.append(msg)
 
     try:
