09e86cce0ac1cf82b4dda6c029515b0fd7b8cc4f
Timo Weiss <timo.weiss@protonmail.ch>
2025-10-15 20:44:37 +0200

    Reordered API

--- apps/api/src/backend/worker.py
+++
@@ -1,6 +0,0 @@
-from rq import Worker
-from .queue import redis_conn
-from . import jobs  # ensure callables imported
-
-if __name__ == "__main__":
-    Worker(["orchestrator", "suggest", "check"], connection=redis_conn).work()
