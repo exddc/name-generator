e2c96c81cb0492bb87ac7756d3b9d76661440e2c
Timo Weiss <timo.weiss@protonmail.ch>
2025-11-09 11:18:40 +0100

    Reworked header button design

--- apps/web/components/ui/navigation-menu.tsx
+++ apps/web/components/ui/navigation-menu.tsx
@@ -41,7 +41,7 @@ NavigationMenuList.displayName = NavigationMenuPrimitive.List.displayName;
 const NavigationMenuItem = NavigationMenuPrimitive.Item;
 
 const navigationMenuTriggerStyle = cva(
-    'group inline-flex h-9 w-max items-center justify-center rounded-lg bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[state=open]:text-accent-foreground data-[state=open]:bg-accent/50 data-[state=open]:hover:bg-accent data-[state=open]:focus:bg-accent'
+    'group hover:backdrop-blur-lg inline-flex h-9 w-max items-center justify-center rounded-lg bg-transparent px-4 py-2 text-sm font-medium transition-all duration-300 border border-transparent hover:bg-white hover:text-black focus:bg-white hover:shadow-sm focus:shadow-sm hover:bg-opacity-50 hover:border-neutral-300 focus:text-black focus:outline-none focus:border-neutral-300 disabled:pointer-events-none disabled:opacity-50 data-[state=open]:text-accent-foreground data-[state=open]:bg-accent/50 data-[state=open]:hover:bg-accent data-[state=open]:focus:bg-accent'
 );
 
 const NavigationMenuTrigger = React.forwardRef<
