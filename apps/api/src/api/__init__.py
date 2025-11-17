3baaf6ccaf0ae0cdd14eb88087c20c865ade0d23
Timo Weiss <timo.weiss@protonmail.ch>
2025-11-17 21:04:42 +0100

    updated api dockerfile to hopefully run in prod

--- apps/api/src/api/__init__.py
+++ apps/api/src/api/__init__.py
@@ -1,5 +1,8 @@
-from importlib.metadata import version
+from importlib.metadata import PackageNotFoundError, version
 
 __title__ = "Domain Generator API"
 __description__ = "API for the Domain Generator service"
-__version__ = version("api")
+try:
+    __version__ = version("api")
+except PackageNotFoundError:
+    __version__ = "0.1.0"
